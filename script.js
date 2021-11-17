f91820fae95208fd262b09c203f14a0a9dca6e53
tovebr <tove.brandt@student.kyh.se>
2021-11-17 11:14:18 +0100

    almost done

---
+++ script.js
@@ -0,0 +1,165 @@
+const todoHeaderInput = document.getElementById("todo-header");
+const todoDeadlineInput = document.getElementById("todo-deadline");
+const todoSubmitInput = document.getElementById("todo-submit");
+const todosDisplay = document.getElementById("display-todos");
+const sortDeadline = document.querySelector(".sort-deadline");
+const sortCreated = document.querySelector(".sort-created");
+
+class Todo {
+  constructor(id, todoHeader, deadline, creationDate, done = false) {
+    (this.id = id),
+      (this.todoHeader = todoHeader),
+      (this.deadline = deadline),
+      (this.creationDate = creationDate),
+      (this.done = done);
+  }
+}
+
+const todos = [];
+
+// TEST DATA
+/* todos.push(
+  new Todo(0, "Marsvin", "2021-04-12", "2021-11-04"),
+  new Todo(3, "Katt", "2021-09-12", "2021-11-10"),
+  new Todo(6, "Äpple", "2021-09-08", "2021-11-18"),
+  new Todo(7, "Gräslög", "2021-06-18", "2021-11-20")
+); */
+
+// Saving a new todo
+todoSubmitInput.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //check that something was entered
+  if (todoHeaderInput.value) {
+    //save the date of creation
+    const creationDate = new Date();
+    // string creationdate
+    const creationDateShort = `${creationDate.getFullYear()}-${
+      creationDate.getMonth() + 1
+    }-${creationDate.getDate()}`;
+
+    //create id
+    let id = null;
+    if (todos.length === 0) {
+      id = 0;
+    } else if (todos.length === 1) {
+      id = todos[0].id + 1;
+    } else {
+      id = todos.map((todo) => todo.id).reduce((a, b) => (a > b ? a : b)) + 1;
+    }
+    //save new todo in global array
+    todos.push(
+      new Todo(
+        id,
+        todoHeaderInput.value,
+        todoDeadlineInput.value,
+        creationDateShort
+      )
+    );
+    //print new todo in browser
+    printCard(todos[todos.length - 1]);
+  }
+
+  //empty input-fields
+  todoHeaderInput.value = "";
+  todoDeadlineInput.value = "";
+});
+
+// function print new todo in browser
+function printCard(todo) {
+  const html = `<div id="${todo.id}" class="card">
+          <div class="card-header">
+            <h3>${todo.todoHeader}</h3>
+          </div>
+          <div class="date-container deadline">
+            <p>Deadline</p>
+            <p class="date-display">${todo.deadline}</p>
+          </div>
+          <div class="date-container">
+            <p>Created</p>
+            <p class="date-display">${todo.creationDate}</p>
+          </div>
+          <div class="card-buttons">
+            <button class="btn-done">Done</button>
+            <i class="fas fa-edit"></i>
+            <i class="fas fa-trash"></i>
+          </div>
+        </div>`;
+
+  todosDisplay.insertAdjacentHTML("afterbegin", html);
+}
+
+// Print testdata
+/* todos.forEach((e) => printCard(e)); */
+
+//eventlistener that hears and directs clicks to right function
+todosDisplay.addEventListener("click", (e) => {
+  const { target } = e;
+  const element = target.parentNode.parentNode;
+  //if edi-icon is clicked
+  if (target.classList.contains("fas")) {
+    editTodo([element.id, target.classList.value]);
+    //if done-icon is clicked
+  } else if (target.classList.contains("btn-done")) {
+    todoDone(element);
+  }
+});
+
+function editTodo(element) {
+  //function recives data on what should be edited
+  const [id, classes] = element;
+  //finds index of target
+  const index = todos.findIndex((todo) => todo.id === Number(id));
+
+  // displays obj data in inputs for editing
+  if (classes.includes("edit")) {
+    todoHeaderInput.value = todos[index].todoHeader;
+    todoDeadlineInput.value = todos[index].deadline;
+  }
+
+  // deletes obj in browser and in array
+  deleteUICard(id);
+  todos.splice(index, 1);
+}
+
+// deletes UI representation of obj
+function deleteUICard(id) {
+  const todo = document.getElementById(id);
+  todo.parentNode.removeChild(todo);
+}
+
+// toggles done-class on obj if done-button is pressed
+function todoDone(element) {
+  element.querySelector(".card-header").classList.toggle("done");
+  const index = todos.findIndex((todo) => {
+    return todo.id === Number(element.id);
+  });
+  todos[index].done = true;
+  /* sortTodos("done"); */
+}
+
+sortDeadline.addEventListener("click", (e) => {
+  e.preventDefault();
+  //create sortable dates for all todos
+  todos.forEach((el) => {
+    el.deadlineSortable = el.deadline.split("-").join("");
+  });
+  sortTodos("deadlineSortable");
+});
+
+sortCreated.addEventListener("click", (e) => {
+  e.preventDefault();
+  sortTodos("id");
+});
+
+function sortTodos(sortBy) {
+  todos.sort((a, b) => b[sortBy] - a[sortBy]);
+  reprintTodos();
+}
+
+function reprintTodos() {
+  todos.forEach((el) => {
+    deleteUICard(el.id);
+    printCard(el);
+  });
+}
