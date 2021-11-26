f620e6f80ccf62933859c5b7a04a40615132df55
tovebr <tove.brandt@student.kyh.se>
2021-11-26 11:53:42 +0100

    ITS WORKING!!

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const todoHeaderInput = document.getElementById("todo-header");
 const todoDeadlineInput = document.getElementById("todo-deadline");
 const todoSubmitInput = document.getElementById("todo-submit");
 const todosDisplay = document.getElementById("display-todos");
+const doneTodosDisplay = document.getElementById("display-done-todos");
 const sortDeadline = document.querySelector(".sort-deadline");
 const sortCreated = document.querySelector(".sort-created");
 
@@ -15,10 +16,13 @@ class Todo {
   }
 }
 
-const todos = [];
+let allTodos = {
+  doneTodos: [],
+  todos: [],
+};
 
 // TEST DATA
-/* todos.push(
+/* allTodos.todos.push(
   new Todo(0, "Marsvin", "2021-04-12", "2021-11-04"),
   new Todo(3, "Katt", "2021-09-12", "2021-11-10"),
   new Todo(6, "Äpple", "2021-09-08", "2021-11-18"),
@@ -40,15 +44,18 @@ todoSubmitInput.addEventListener("click", (e) => {
 
     //create id
     let id = null;
-    if (todos.length === 0) {
+    if (allTodos.todos.length === 0) {
       id = 0;
-    } else if (todos.length === 1) {
-      id = todos[0].id + 1;
+    } else if (allTodos.todos.length === 1) {
+      id = allTodos.todos[0].id + 1;
     } else {
-      id = todos.map((todo) => todo.id).reduce((a, b) => (a > b ? a : b)) + 1;
+      id =
+        allTodos.todos
+          .map((todo) => todo.id)
+          .reduce((a, b) => (a > b ? a : b)) + 1;
     }
     //save new todo in global array
-    todos.push(
+    allTodos.todos.push(
       new Todo(
         id,
         todoHeaderInput.value,
@@ -57,7 +64,7 @@ todoSubmitInput.addEventListener("click", (e) => {
       )
     );
     //print new todo in browser
-    printCard(todos[todos.length - 1]);
+    printCard(allTodos.todos[allTodos.todos.length - 1]);
   }
 
   //empty input-fields
@@ -67,6 +74,8 @@ todoSubmitInput.addEventListener("click", (e) => {
 
 // function print new todo in browser
 function printCard(todo) {
+  localStorage.setItem("allTodos", JSON.stringify(allTodos));
+
   const html = `<div id="${todo.id}" class="card">
           <div class="card-header">
             <h3>${todo.todoHeader}</h3>
@@ -86,40 +95,57 @@ function printCard(todo) {
           </div>
         </div>`;
 
-  todosDisplay.insertAdjacentHTML("afterbegin", html);
+  if (todo.done) {
+    doneTodosDisplay.insertAdjacentHTML("afterbegin", html);
+    const cardDiv = document.getElementById(todo.id);
+    cardDiv.classList.toggle("done");
+    cardDiv.querySelector(".card-header").classList.toggle("done-header");
+  } else {
+    todosDisplay.insertAdjacentHTML("afterbegin", html);
+  }
 }
 
 // Print testdata
-/* todos.forEach((e) => printCard(e)); */
+/* allTodos.todos.forEach((e) => printCard(e)); */
 
 //eventlistener that hears and directs clicks to right function
-todosDisplay.addEventListener("click", (e) => {
+todosDisplay.addEventListener("click", (e) => handleClicks(e));
+doneTodosDisplay.addEventListener("click", (e) => handleClicks(e));
+
+function handleClicks(e) {
   const { target } = e;
   const element = target.parentNode.parentNode;
+  const currTodoArray = element.classList.contains("done")
+    ? "doneTodos"
+    : "todos";
+
   //if edi-icon is clicked
   if (target.classList.contains("fas")) {
-    editTodo([element.id, target.classList.value]);
+    editTodo([element.id, currTodoArray, target.classList.value]);
     //if done-icon is clicked
   } else if (target.classList.contains("btn-done")) {
-    todoDone(element);
+    todoDone([element, currTodoArray]);
   }
-});
+}
 
 function editTodo(element) {
   //function recives data on what should be edited
-  const [id, classes] = element;
+  const [id, currTodoArray, targetClasses] = element;
+
   //finds index of target
-  const index = todos.findIndex((todo) => todo.id === Number(id));
+  const index = allTodos[currTodoArray].findIndex(
+    (todo) => todo.id === Number(id)
+  );
 
   // displays obj data in inputs for editing
-  if (classes.includes("edit")) {
-    todoHeaderInput.value = todos[index].todoHeader;
-    todoDeadlineInput.value = todos[index].deadline;
+  if (targetClasses.includes("edit")) {
+    todoHeaderInput.value = allTodos[currTodoArray][index].todoHeader;
+    todoDeadlineInput.value = allTodos[currTodoArray][index].deadline;
   }
 
   // deletes obj in browser and in array
   deleteUICard(id);
-  todos.splice(index, 1);
+  allTodos[currTodoArray].splice(index, 1);
 }
 
 // deletes UI representation of obj
@@ -130,18 +156,34 @@ function deleteUICard(id) {
 
 // toggles done-class on obj if done-button is pressed
 function todoDone(element) {
-  element.querySelector(".card-header").classList.toggle("done");
-  const index = todos.findIndex((todo) => {
-    return todo.id === Number(element.id);
-  });
-  todos[index].done = true;
-  /* sortTodos("done"); */
+  const [uiElement, currTodoArray] = element;
+  let index, clickedTodo;
+
+  index = allTodos[currTodoArray].findIndex(
+    (todo) => todo.id === Number(uiElement.id)
+  );
+
+  clickedTodo = allTodos[currTodoArray][index];
+
+  if (currTodoArray === "todos") {
+    clickedTodo.done = true;
+    allTodos.doneTodos.push(clickedTodo);
+    allTodos.todos.splice(index, 1);
+    deleteUICard(clickedTodo.id);
+  } else {
+    clickedTodo.done = false;
+    allTodos.todos.push(clickedTodo);
+    allTodos.doneTodos.splice(index, 1);
+    deleteUICard(clickedTodo.id);
+  }
+
+  printCard(clickedTodo);
 }
 
 sortDeadline.addEventListener("click", (e) => {
   e.preventDefault();
   //create sortable dates for all todos
-  todos.forEach((el) => {
+  allTodos.todos.forEach((el) => {
     el.deadlineSortable = el.deadline.split("-").join("");
   });
   sortTodos("deadlineSortable");
@@ -153,13 +195,25 @@ sortCreated.addEventListener("click", (e) => {
 });
 
 function sortTodos(sortBy) {
-  todos.sort((a, b) => b[sortBy] - a[sortBy]);
+  allTodos.todos.sort((a, b) => b[sortBy] - a[sortBy]);
   reprintTodos();
 }
 
 function reprintTodos() {
-  todos.forEach((el) => {
+  allTodos.todos.forEach((el) => {
     deleteUICard(el.id);
     printCard(el);
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ref = localStorage.getItem("allTodos");
+  if (ref) {
+    allTodos = JSON.parse(ref);
+    for (const todoArr of Object.entries(allTodos)) {
+      todoArr[1]?.forEach((t) => {
+        printCard(t);
+      });
+    }
+  }
+});
